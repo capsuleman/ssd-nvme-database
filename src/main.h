da3b119f3913c58f261c628f3cd792246a035c56
Guillaume Vagner <guillaume.vagner@gmail.com>
2021-03-11 09:53:57 +0100

    🩹 Increased chunk size

--- src/main.h
+++ src/main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#define CHUNK_SIZE 8 // Number of ELEMENT (not size) in a chunk
+#define CHUNK_SIZE (128 * 1024) // = Number of ELEMENT (not size) in a chunk
 
 #endif
