9bd0990bedc293b0537f97c01aeea422423a2ffd
Thierry <thierry.tio@student.ecp.fr>
2021-03-16 17:42:21 +0100

    implement multi load on findrows

--- benchtest/main.cpp
+++ benchtest/main.cpp
@@ -14,7 +14,6 @@ static void BM_TableWrite(benchmark::State& state) {
     }
 }
 // Register the function as a benchmark
-BENCHMARK(BM_TableWrite);
 
 static void BM_TableRead(benchmark::State& state) {
   const int table_size = 16 * 16;
@@ -32,5 +31,25 @@ static void BM_TableRead(benchmark::State& state) {
     }
 }
 
+static void BM_TableSearch(benchmark::State& state) {
+  const int table_size = 16 * 16;
+    Table table{3, 2};
+    for (int i = 0; i < table_size; i++)
+    {
+        table.writeInt(i, 0, 1);
+        table.writeInt(i, 1, i);
+        table.writeInt(i, 2, 2 * i);
+        table.writeDouble(i, 3, i + 0.1);
+        table.writeDouble(i, 4, i * 1.0);
+    }
+    for (auto _ : state) {
+      int attribute_predicates[3] = {1, 2, 4};
+      double value_predicates[2] = {2.1, 2.0};
+      auto result = table.findRows(attribute_predicates, value_predicates);
+    };
+}
+
+BENCHMARK(BM_TableWrite);
 BENCHMARK(BM_TableRead);
+BENCHMARK(BM_TableSearch);
 BENCHMARK_MAIN();
--- src/Column.cpp
+++ src/Column.cpp
@@ -87,14 +87,21 @@ void Column::writeDoubles(
 std::vector<std::bitset<CHUNK_SIZE>> Column::findIntRows(int predicate)
 {
     std::vector<std::bitset<CHUNK_SIZE>> result = std::vector<std::bitset<CHUNK_SIZE>>(chunks.size());
-    for (unsigned long int chunk_no = 0; chunk_no < chunks.size(); chunk_no++)
-    {
-        chunks[chunk_no].load();
-        for (unsigned long int chunk_pos = 0; chunk_pos < CHUNK_SIZE; chunk_pos++)
-        {
-            result[chunk_no][chunk_pos] = chunks[chunk_no].readInt(chunk_pos) == predicate;
+    for (unsigned long int chunk_no = 0; chunk_no < chunks.size(); chunk_no += ROW_CHUNK_LOAD_N)
+    {   
+        // make parallel
+        for (unsigned long int i = 0; ((chunk_no + i < chunks.size()) && (i < ROW_CHUNK_LOAD_N)); i++) {
+            chunks[chunk_no + i].load();
         }
-        chunks[chunk_no].unload();
+        for (unsigned long int i = 0; ((chunk_no + i < chunks.size()) && (i < ROW_CHUNK_LOAD_N)); i++) {
+            for (unsigned long int chunk_pos = 0; chunk_pos < CHUNK_SIZE; chunk_pos++)
+            {
+                result[chunk_no + i][chunk_pos] = chunks[chunk_no].readInt(chunk_pos) == predicate;
+            }
+        }
+        for (unsigned long int i = 0; ((chunk_no + i < chunks.size()) && (i < ROW_CHUNK_LOAD_N)); i++) {
+            chunks[chunk_no + i].unload();
+        }
     }
     return result;
 }
@@ -102,14 +109,20 @@ std::vector<std::bitset<CHUNK_SIZE>> Column::findIntRows(int predicate)
 std::vector<std::bitset<CHUNK_SIZE>> Column::findDoubleRows(double predicate)
 {
     std::vector<std::bitset<CHUNK_SIZE>> result = std::vector<std::bitset<CHUNK_SIZE>>(chunks.size());
-    for (unsigned long int chunk_no = 0; chunk_no < chunks.size(); chunk_no++)
-    {
-        chunks[chunk_no].load();
-        for (unsigned long int chunk_pos = 0; chunk_pos < CHUNK_SIZE; chunk_pos++)
-        {
-            result[chunk_no][chunk_pos] = chunks[chunk_no].readDouble(chunk_pos) == predicate;
+    for (unsigned long int chunk_no = 0; chunk_no < chunks.size(); chunk_no += ROW_CHUNK_LOAD_N)
+    {   
+        for (unsigned long int i = 0; ((chunk_no + i < chunks.size()) && (i < ROW_CHUNK_LOAD_N)); i++) {
+            chunks[chunk_no + i].load();
         }
-        chunks[chunk_no].unload();
+        for (unsigned long int i = 0; ((chunk_no + i < chunks.size()) && (i < ROW_CHUNK_LOAD_N)); i++) {
+            for (unsigned long int chunk_pos = 0; chunk_pos < CHUNK_SIZE; chunk_pos++)
+            {
+                result[chunk_no + i][chunk_pos] = chunks[chunk_no].readDouble(chunk_pos) == predicate;
+            }
+        }
+        for (unsigned long int i = 0; ((chunk_no + i < chunks.size()) && (i < ROW_CHUNK_LOAD_N)); i++) {
+            chunks[chunk_no + i].unload();
+        }
     }
     return result;
 }
