85ac587de9a13f3ffafd53767697dc074007ee18
Guillaume Vagner <guillaume.vagner@gmail.com>
2021-03-16 15:42:07 +0100

    🔥 Removed main content

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,4 @@
-#include <cassert>
-#include <iostream>
-
-#include "Table.h"
-
-// TODO: implement benchmark
-
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
     return 0;
 }
